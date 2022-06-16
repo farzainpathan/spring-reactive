6b3babe9a5b895c36bfca9f27baf48c9367d0230
Farzain Pathan <farzainpathan@gmail.com>
2022-06-17 03:01:45 +0530

    refactor: optimize imports

--- src/main/java/com/example/springreactive/SpringReactiveApplication.java
+++ src/main/java/com/example/springreactive/SpringReactiveApplication.java
@@ -1,13 +1,7 @@
 package com.example.springreactive;
 
-import com.example.springreactive.modal.Product;
-import com.example.springreactive.repository.ProductRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.core.ReactiveMongoOperations;
-import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class SpringReactiveApplication {
